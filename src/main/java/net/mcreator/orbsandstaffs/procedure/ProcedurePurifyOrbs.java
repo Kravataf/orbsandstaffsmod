98a60b36314beee29f6086a1e14e4eb4fc79f165
creat <creat@kravataf.home>
2024-05-26 12:05:36 +0200

    skibidi

--- src/main/java/net/mcreator/orbsandstaffs/procedure/ProcedurePurifyOrbs.java
+++ src/main/java/net/mcreator/orbsandstaffs/procedure/ProcedurePurifyOrbs.java
@@ -1,9 +1,5 @@
 package net.mcreator.orbsandstaffs.procedure;
 
-import net.minecraft.world.World;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.tileentity.TileEntityLockableLoot;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.Container;
@@ -28,27 +24,7 @@ public class ProcedurePurifyOrbs extends ElementsOnS.ModElement {
 			System.err.println("Failed to load dependency entity for procedure PurifyOrbs!");
 			return;
 		}
-		if (dependencies.get("x") == null) {
-			System.err.println("Failed to load dependency x for procedure PurifyOrbs!");
-			return;
-		}
-		if (dependencies.get("y") == null) {
-			System.err.println("Failed to load dependency y for procedure PurifyOrbs!");
-			return;
-		}
-		if (dependencies.get("z") == null) {
-			System.err.println("Failed to load dependency z for procedure PurifyOrbs!");
-			return;
-		}
-		if (dependencies.get("world") == null) {
-			System.err.println("Failed to load dependency world for procedure PurifyOrbs!");
-			return;
-		}
 		Entity entity = (Entity) dependencies.get("entity");
-		int x = (int) dependencies.get("x");
-		int y = (int) dependencies.get("y");
-		int z = (int) dependencies.get("z");
-		World world = (World) dependencies.get("world");
 		if ((((new Object() {
 			public ItemStack getItemStack(int sltid) {
 				if (entity instanceof EntityPlayerMP) {
@@ -62,51 +38,42 @@ public class ProcedurePurifyOrbs extends ElementsOnS.ModElement {
 				}
 				return ItemStack.EMPTY;
 			}
-		}.getItemStack((int) (0))).getItem() == new ItemStack(ItemOrbOfLightning.block, (int) (1)).getItem()) && (((new Object() {
-			public int getAmount(BlockPos pos, int sltid) {
-				TileEntity inv = world.getTileEntity(pos);
-				if (inv instanceof TileEntityLockableLoot) {
-					ItemStack stack = ((TileEntityLockableLoot) inv).getStackInSlot(sltid);
-					if (stack != null)
-						return stack.getCount();
-				}
-				return 0;
-			}
-		}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (1))) == 0) || ((new Object() {
-			public ItemStack getItemStack(int sltid) {
+		}.getItemStack((int) (0))).getItem() == new ItemStack(ItemOrbOfLightning.block, (int) (1)).getItem()) && ((new Object() {
+			public int getAmount(int sltid) {
 				if (entity instanceof EntityPlayerMP) {
 					Container _current = ((EntityPlayerMP) entity).openContainer;
 					if (_current instanceof Supplier) {
 						Object invobj = ((Supplier) _current).get();
 						if (invobj instanceof Map) {
-							return ((Slot) ((Map) invobj).get(sltid)).getStack();
+							ItemStack stack = ((Slot) ((Map) invobj).get(sltid)).getStack();;
+							if (stack != null)
+								return stack.getCount();
 						}
 					}
 				}
-				return ItemStack.EMPTY;
+				return 0;
 			}
-		}.getItemStack((int) (1))).getItem() == new ItemStack(ItemEmptyOrb.block, (int) (1)).getItem())))) {
-			{
-				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
-				if (inv instanceof TileEntityLockableLoot)
-					((TileEntityLockableLoot) inv).removeStackFromSlot((int) (0));
+		}.getAmount((int) (1))) == 0))) {
+			if (entity instanceof EntityPlayerMP) {
+				Container _current = ((EntityPlayerMP) entity).openContainer;
+				if (_current instanceof Supplier) {
+					Object invobj = ((Supplier) _current).get();
+					if (invobj instanceof Map) {
+						((Slot) ((Map) invobj).get((int) (0))).decrStackSize((int) (1));
+						_current.detectAndSendChanges();
+					}
+				}
 			}
-			{
-				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
-				if (inv != null && (inv instanceof TileEntityLockableLoot)) {
-					ItemStack _setstack = new ItemStack(ItemEmptyOrb.block, (int) (1));
-					_setstack.setCount(((new Object() {
-						public int getAmount(BlockPos pos, int sltid) {
-							TileEntity inv = world.getTileEntity(pos);
-							if (inv instanceof TileEntityLockableLoot) {
-								ItemStack stack = ((TileEntityLockableLoot) inv).getStackInSlot(sltid);
-								if (stack != null)
-									return stack.getCount();
-							}
-							return 0;
-						}
-					}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (1))) + 1));
-					((TileEntityLockableLoot) inv).setInventorySlotContents((int) (1), _setstack);
+			if (entity instanceof EntityPlayerMP) {
+				Container _current = ((EntityPlayerMP) entity).openContainer;
+				if (_current instanceof Supplier) {
+					Object invobj = ((Supplier) _current).get();
+					if (invobj instanceof Map) {
+						ItemStack _setstack = new ItemStack(ItemEmptyOrb.block, (int) (1));
+						_setstack.setCount(1);
+						((Slot) ((Map) invobj).get((int) (1))).putStack(_setstack);
+						_current.detectAndSendChanges();
+					}
 				}
 			}
 		}
